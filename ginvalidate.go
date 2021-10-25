c7ceaa9aca6985c36cdfe40d0f15361a25007d51
huanjiesm <huanjiesm@163.com>
2021-10-25 11:36:31 +0800

    query参数

--- ginvalidate.go
+++ ginvalidate.go
@@ -43,3 +43,33 @@ func BindJsonStruct(c *gin.Context, rules map[string]govalidate.FilterItem, obj 
 	}
 	return errCode, nil
 }
+
+// BindQueryMap 解析Query部分参数
+func BindQueryMap(c *gin.Context, rules map[string]govalidate.FilterItem) (map[string]interface{}, int32, error) {
+	tmpRes := make(map[string]interface{})
+	values := c.Request.URL.Query()
+	for k, v := range values {
+		if len(v) == 1 {
+			tmpRes[k] = v[0]
+		}
+		tmpRes[k] = v
+	}
+	res, errCode, err := govalidate.Validate(tmpRes, rules)
+	if err != nil {
+		return nil, 0, err
+	}
+	return res, errCode, nil
+}
+
+// BindQueryStruct 解析Query参数
+func BindQueryStruct(c *gin.Context, rules map[string]govalidate.FilterItem, obj interface{}) (int32, error) {
+	res, errCode, err := BindQueryMap(c, rules)
+	if err != nil {
+		return 0, err
+	}
+	err = mapstructure.Decode(res, obj)
+	if err != nil {
+		return 0, err
+	}
+	return errCode, nil
+}
